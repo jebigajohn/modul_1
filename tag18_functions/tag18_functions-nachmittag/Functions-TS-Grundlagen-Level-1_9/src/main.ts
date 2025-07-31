6bea1c3ef2d9cd055a01549393bc0cb323ea5c20
Johnstb <jstaub8596@gmail.com>
2025-07-31 07:52:47 +0700

    functions

--- tag18_functions/tag18_functions-nachmittag/Functions-TS-Grundlagen-Level-1_9/src/main.ts
+++ tag18_functions/tag18_functions-nachmittag/Functions-TS-Grundlagen-Level-1_9/src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+function myBio(vorname: string, nachname: string, geburtsort: string, alter: number, wohnort: string): void {
+  const fullNameOutput = `Mein Name ist ${vorname} ${nachname}`
+  const birthPlaceOutput = `Ich bin in ${geburtsort} geboren. `
+  const ageOutput = `Ich bin ${alter} Jahre alt.`
+  const residenceOutput = `Ich wohne in ${wohnort}`
+  const bioText = `Mein Name ist ${vorname} ${nachname}. Ich bin in ${geburtsort} geboren, ${alter} Jahre alt und wohne in ${wohnort}.`
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  console.log(bioText)
+  console.log(fullNameOutput)
+  console.log(birthPlaceOutput)
+  console.log(ageOutput)
+  console.log(residenceOutput)
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+myBio("John", "Staub", "Beckum", 29, "Diestedde")
