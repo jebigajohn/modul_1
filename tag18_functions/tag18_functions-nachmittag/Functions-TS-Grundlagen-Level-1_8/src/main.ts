6547689544c54ac9503aef948cd51c787a07630b
Johnstb <jstaub8596@gmail.com>
2025-07-31 07:54:00 +0700

    functionsfull

--- tag18_functions/tag18_functions-nachmittag/Functions-TS-Grundlagen-Level-1_8/src/main.ts
+++ tag18_functions/tag18_functions-nachmittag/Functions-TS-Grundlagen-Level-1_8/src/main.ts
@@ -1,24 +1,18 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+function myAge(birthYear: Date): number {
+  const heute = new Date()
+  const geburtsJahr = birthYear.getFullYear()
+  const aktuellesJahr = heute.getFullYear()
+  let alter = aktuellesJahr - geburtsJahr
+  return alter
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+console.log(myAge(new Date("1996-08-05")))
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function alterVergleichen(meinAlter: number, anderesAlter: number): number {
+  const altersDifferenz = Math.abs(meinAlter - anderesAlter)
+  return altersDifferenz
+}
+
+const differenz = alterVergleichen(29, 34)
+
+console.log(differenz)
